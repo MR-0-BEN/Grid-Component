35730626df87a8edd1bdc79a6067c5370aceaa6d
MR.BeN <anas.mohamad.official@gmail.com>
2023-09-19 19:34:14 +0300

    fix tailwind sorrt

---
+++ components/Grid.jsx
@@ -0,0 +1,8 @@
+const Grid = () => {
+  return (
+    <section className="text-center text-4xl flex capitalize w-[100vw] h-[100vh] bg-[#e5eff5]">
+      grid
+    </section>
+  );
+};
+export default Grid;
